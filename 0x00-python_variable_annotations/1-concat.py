935a0e644a27c0cd5f0c6f5a13ae1bfeeef16791
KenRyan <122825337+KennedyRyan-code@users.noreply.github.com>
2024-01-15 13:18:18 +0300

    Basic annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" 
+a type-annotated function that takes a string str1 and str2 as arguments
+and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ returns a concat string """
+    return (str1 + str2)
